c4d52e0c8920ddff8f530d351556953fc481f5bc
chiduralanikhil <chiduralanikhil23@gmail.com>
2023-12-10 19:15:34 +0530

    added new field  id in book schema

--- Models/bookModel.js
+++ Models/bookModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 
 const bookSchema = new mongoose.Schema({
+    bookId: {type: Number, required : true },
     title: { type: String, required: true },
     author: { type: String, required: true },
     published_date: { type: String }
